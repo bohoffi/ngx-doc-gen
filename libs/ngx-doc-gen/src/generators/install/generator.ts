68859346caf07168332540add310c3acd5668668
bohoffi <bo.hoffi@gmail.com>
2022-09-27 17:41:28 +0200

    refactor: configure all buildable libs if non where specified

--- libs/ngx-doc-gen/src/generators/install/generator.ts
+++ libs/ngx-doc-gen/src/generators/install/generator.ts
@@ -6,7 +6,11 @@ export default async function install(
   installOptions: InstallGeneratorOptions
 ): Promise<void | GeneratorCallback> {
 
+  logger.info('Fetching workspace projects...');
+
   const workspaceProjects = getProjects(tree);
+
+  logger.info('Choosing buildable libraries...');
 
   const workspaceLibraries = Array.from(workspaceProjects.entries()).filter(([, projectConfiguration]) => {
     return projectConfiguration.projectType === 'library' && typeof projectConfiguration.targets?.build === 'object';
@@ -17,12 +21,14 @@ export default async function install(
   // check if a project name was given which does not exist in workspace libraries
   const nonExistingProjects = optionsProjects.filter(optionsProject => !workspaceLibraries.map(([projectName]) => projectName).includes(optionsProject));
   if ((nonExistingProjects).some(p => p)) {
-    throw new Error(
-      `❌ You specified projects which do not exist in workspace: "${nonExistingProjects.join(', ')}".`
-    );
+    throw new Error(`❌ You specified projects which do not exist in workspace: "${nonExistingProjects.join(', ')}".`);
   }
 
-  const specifiedLibraries = workspaceLibraries.filter(([projectName]) => optionsProjects.includes(projectName));
+  if (optionsProjects.length) {
+    logger.info(`Filter for specified libraries: "${optionsProjects.join(', ')}"`);
+  }
+
+  const specifiedLibraries = optionsProjects.length ? workspaceLibraries.filter(([projectName]) => optionsProjects.includes(projectName)) : workspaceLibraries;
 
   specifiedLibraries.forEach(([projectName, projectConfiguration]) => {
     if (projectConfiguration.targets) {
