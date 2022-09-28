580e294f1c23c156793df3e5f80fc500ff0647ff
bohoffi <bo.hoffi@gmail.com>
2022-09-28 20:24:26 +0200

    refactor: clean up
    * remove custom implementations of `ng-packagr` interfaces

--- libs/ngx-doc-gen/src/interfaces/ng-package.ts
+++
@@ -1,9 +0,0 @@
-import { NgEntrypoint } from './ng-entrypoint';
-
-export interface NgPackage {
-  basePath: string;
-  version?: string;
-  packageName: string;
-  primary: NgEntrypoint;
-  secondaries: NgEntrypoint[];
-}
