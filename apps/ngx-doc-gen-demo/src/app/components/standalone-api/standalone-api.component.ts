4f8ca5eec383b753c59a4fdc2bbf79182d2d25d8
bohoffi <bo.hoffi@gmail.com>
2022-12-28 12:01:42 +0100

    doc: update the demo

---
+++ apps/ngx-doc-gen-demo/src/app/components/standalone-api/standalone-api.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-doc-gen-standalone-api',
+  templateUrl: './standalone-api.html',
+  styleUrls: ['./standalone-api.component.scss'],
+})
+export class StandaloneApiComponent {}
