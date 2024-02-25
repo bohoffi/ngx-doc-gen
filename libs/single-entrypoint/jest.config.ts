ffaeb60ea8ed71630bfe0adce726522cb6ecb789
bohoffi <bo.hoffi@gmail.com>
2024-02-25 09:04:24 +0100

    chore: [nx migration] update-configs-jest-29

--- libs/single-entrypoint/jest.config.ts
+++ libs/single-entrypoint/jest.config.ts
@@ -3,15 +3,13 @@ export default {
   displayName: 'single-entrypoint',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    }
-  },
+  globals: {  },
   coverageDirectory: '../../coverage/libs/single-entrypoint',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
+    '^.+\\.(ts|mjs|js|html)$': ['jest-preset-angular', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    }]
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
